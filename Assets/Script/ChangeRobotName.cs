f55b38565dbff7d14361a2cade8b316b158f8790
GyuYeong <77821573+SimGyuYeong@users.noreply.github.com>
2021-12-01 01:01:42 +0900

    첫번쨰 선택지 작업

---
+++ Assets/Script/ChangeRobotName.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRobotName : MonoBehaviour
+{
+    [SerializeField] private Fungus.Character Name;
+
+    public void NameChange()
+    {
+        Name.nameText = "·Îº¿";
+        Destroy(gameObject);
+    }
+}
